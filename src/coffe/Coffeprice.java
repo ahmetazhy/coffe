20f849795362c669c6b288e326ef3e48e54e4473
Ahmed <ahmad.azhy2233@gmail.com>
2021-10-05 02:31:30 +0300

    change String to upper

--- src/coffe/Coffeprice.java
+++ src/coffe/Coffeprice.java
@@ -1,68 +1,55 @@
 package coffe;
 
-
-
-
+import javax.print.DocFlavor.STRING;
 
 public class Coffeprice {
-	
-		private double iCaffe_Americano;
-		private double iMochaccino;
-		private double icappuccino;
-		private double iIrish_Coffee;
-		private double ilong_black_coffee;
-		private string text;
-		
-		
-		
 
-		public double getiMochaccino() {
-			return iMochaccino;
-		}
+	private double iCaffe_Americano;
+	private double iMochaccino;
+	private double icappuccino;
+	private double iIrish_Coffee;
+	private double ilong_black_coffee;
 
-		public void setiMochaccino(double iMochaccino) {
-			this.iMochaccino = iMochaccino;
-		}
+	private STRING text;
 
-		public double getiCaffe_Americano() {
-			return iCaffe_Americano;
-		}
-
-		public void setiCaffe_Americano(double iCaffe_Americano) {
-			this.iCaffe_Americano = iCaffe_Americano;
-		}
-
-		public double getIcappuccino() {
-			return icappuccino;
-		}
-
-		public void setIcappuccino(double icappuccino) {
-			this.icappuccino = icappuccino;
-		}
-
-		public double getiIrish_Coffee() {
-			return iIrish_Coffee;
-		}
-
-		public void setiIrish_Coffee(double iIrish_Coffee) {
-			this.iIrish_Coffee = iIrish_Coffee;
-		}
-
-		public double getIlong_black_coffee() {
-			return ilong_black_coffee;
-		}
-
-		public void setIlong_black_coffee(double ilong_black_coffee) {
-			this.ilong_black_coffee = ilong_black_coffee;
-		}
-		
-		
-		
-		
-		
-		
-
+	public double getiMochaccino() {
+		return iMochaccino;
 	}
 
+	public void setiMochaccino(double iMochaccino) {
+		this.iMochaccino = iMochaccino;
+	}
 
+	public double getiCaffe_Americano() {
+		return iCaffe_Americano;
+	}
 
+	public void setiCaffe_Americano(double iCaffe_Americano) {
+		this.iCaffe_Americano = iCaffe_Americano;
+	}
+
+	public double getIcappuccino() {
+		return icappuccino;
+	}
+
+	public void setIcappuccino(double icappuccino) {
+		this.icappuccino = icappuccino;
+	}
+
+	public double getiIrish_Coffee() {
+		return iIrish_Coffee;
+	}
+
+	public void setiIrish_Coffee(double iIrish_Coffee) {
+		this.iIrish_Coffee = iIrish_Coffee;
+	}
+
+	public double getIlong_black_coffee() {
+		return ilong_black_coffee;
+	}
+
+	public void setIlong_black_coffee(double ilong_black_coffee) {
+		this.ilong_black_coffee = ilong_black_coffee;
+	}
+
+}
